6851cc411a12ede12c5fb30131dead3bd6c0c535
Furjii <furjimadi@gmail.com>
2023-12-22 19:30:41 +0700

    done for today meet 13 PAM

--- app/src/main/java/com/example/aplikasikontak/model/Kontak.kt
+++ app/src/main/java/com/example/aplikasikontak/model/Kontak.kt
@@ -1,11 +1,14 @@
 package com.example.aplikasikontak.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Kontak(
     val id: Int,
     val nama: String,
+    @SerialName("email")
     val alamat: String,
+    @SerialName("nohp")
     val telpon: String,
 )
--- app/src/main/java/com/example/aplikasikontak/repository/KontakRepository.kt
+++ app/src/main/java/com/example/aplikasikontak/repository/KontakRepository.kt
@@ -2,11 +2,20 @@ package com.example.aplikasikontak.repository
 
 import com.example.aplikasikontak.model.Kontak
 import com.example.aplikasikontak.service_api.KontakService
+import java.io.IOException
 
 interface KontakRepository {
     /** Fetches List of Kontak from kontakApi */
 
     suspend fun getKontak(): List<Kontak>
+
+    suspend fun insertkontak(kontak: Kontak)
+
+    suspend fun updatekontak(id: Int, kontak: Kontak)
+
+    suspend fun deletekontak(id: Int)
+
+    suspend fun getkontakById(id: Int): Kontak
 }
 
 class NetworkKontakRepository(
@@ -15,4 +24,34 @@ class NetworkKontakRepository(
     /** Fetches List of Kontak from KontakApi*/
 
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+
+    override suspend fun insertkontak(kontak: Kontak) {
+        kontakApiService.insertKontak(kontak)
+
+    }
+
+    override suspend fun updatekontak(id: Int, kontak: Kontak) {
+        kontakApiService.updateKontak(id, kontak)
+    }
+
+    override suspend fun deletekontak(id: Int) {
+        try {
+            val response = kontakApiService.deleteKontak(id)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete kontak. HTTP status code :" +
+                " ${response.code()}")
+            }
+            else{
+                response.message()
+                println(response.message())
+            }
+        }
+        catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getkontakById(id: Int): Kontak {
+        return kontakApiService.getKontakById(id)
+    }
 }
