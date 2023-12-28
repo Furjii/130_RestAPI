428e22c7cca630978ae66fc9b44fa0c9c9e1b3ef
Furjii <furjimadi@gmail.com>
2023-12-28 19:34:04 +0700

    menambahkan function TopAppBar

--- app/src/main/java/com/example/aplikasikontak/ui/KontakApp.kt
+++ app/src/main/java/com/example/aplikasikontak/ui/KontakApp.kt
@@ -5,8 +5,12 @@ import androidx.annotation.RequiresExtension
 import com.example.aplikasikontak.R
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -63,4 +67,32 @@ fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modi
         modifier = modifier
 
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarKontak(
+    title: String,
+    canNavigateBack: Boolean,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    modifier: Modifier = Modifier,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = { Text(title) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "")
+
+                }
+            }
+        }
+
+    )
+
 }
