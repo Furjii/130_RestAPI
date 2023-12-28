3956e170c4fd08a65cd8ff3e3bc5f3b47c0d3985
Furjii <furjimadi@gmail.com>
2023-12-28 21:13:49 +0700

    Menambahkan nohp textfield

--- app/src/main/java/com/example/aplikasikontak/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/aplikasikontak/ui/home/screen/InsertScreen.kt
@@ -3,11 +3,15 @@ package com.example.aplikasikontak.ui.home.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.aplikasikontak.ui.home.viewmodel.InsertUiEvent
 
@@ -39,6 +43,26 @@ fun FormInputSiswa(
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nohp,
+            onValueChange = {onValueChange(insertUiEvent.copy(nohp = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = "No HP")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if (enabled){
+            Text(text = "Isi semua data",
+                modifier = Modifier.padding(start = 12.dp)
+            )
+        }
+
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
         )
 
     }
